4a32b1b590f9b39b2d193cf2821e1ea4345573b4
Vikram-Agentic <Vikram-Agenticai@users.noreply.github.com>
2026-07-23 17:37:18 +0530

    Revert "Add VranceFlex production application"
    This reverts commit dcbc59c83d7bb17b57b38d992b3423fa54b8dac6.

--- vranceflex/app/session-tasks/choose-organization/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function ChooseOrganizationPage() {
-  redirect("/dashboard");
-}
--- vranceflex/lib/server/auth-errors.ts
+++
@@ -1,8 +0,0 @@
-export class AuthRequestError extends Error {
-  constructor(
-    message: string,
-    readonly status: 400 | 401 | 403 | 409 | 429,
-  ) {
-    super(message);
-  }
-}
